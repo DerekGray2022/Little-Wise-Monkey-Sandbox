96169fb8695d98291d60db249eef890704c98e46
Derek Gray <105393523+DerekGray2022@users.noreply.github.com>
2024-03-01 16:59:34 +0000

    Page 2 Initial Development

--- js/gsap.js
+++ js/gsap.js
@@ -1,9 +1,64 @@
+gsap.registerPlugin(ScrollTrigger);
+
+//      onLoad Animation
 let tl = gsap.timeline();
-tl.to('#header', { opacity: 1, y: 100, duration: 3, ease: 'inOut' });
-tl.to('#about', { opacity: 1, x: 200 });
-tl.to('#employment', { opacity: 1, x: 200 });
-tl.to('#projects', { opacity: 1, x: 200 });
-tl.to('.intro', { opacity: 1, duration: 6 });
+tl.to('#header', { opacity: 1, y: 100, duration: 2, ease: 'inOut' });
+tl.to('#about', { opacity: 1, x: 200, ease: "elastic", duration: 3 });
+tl.to('#employment', { opacity: 1, x: 200, ease: "elastic", duration: 3 }, "-=2.5");
+tl.to('#projects', { opacity: 1, x: 200, ease: "elastic", duration: 3 }, "-=2.5");
+tl.to('.intro', { opacity: 1, y: -50, duration: 2 }, "-=2.5");
+
+
+//      Scroll Animation
+ScrollTrigger.matchMedia({
+
+    "(max-width: 320px)": () => {
+        let tl = gsap.timeline({
+            scrollTrigger: {
+                trigger: ".div2",
+                // markers: true,
+                start: "top 90%",
+                end: "top top",
+                scrub: 4,
+                isTouch: 2
+            }
+        });
+        tl.to('.intro p', {
+            y: 350,
+            x: -20,
+            duration: 5,
+            ease: "ease-out",
+            color: "#a9029e",
+            width: "80vw",
+        })
+        .to('.hidden-xsm', { opacity: 1, duration: 4 })
+        .to('.div1 h1', {opacity: 1, duration: 8, ease: "ease-in-out"})
+    },
+
+    "(min-width: 321px)": () => {
+        let tl = gsap.timeline({
+            scrollTrigger: {
+                trigger: ".div2",
+                // markers: true,
+                start: "top 90%",
+                end: "top top",
+                scrub: 4,
+                isTouch: 2
+            }
+        });
+        tl.to('.intro p', {
+            y: 350,
+            x: -20,
+            duration: 5,
+            ease: "ease-in-out",
+            color: "#a9029e",
+            width: "80vw",
+        })
+        .to('.hidden-xsm', { opacity: 1, duration: 4, ease: "ease-in-out" },"+=4")
+        .to('.hidden-sm', { display: "block",opacity: 1, duration: 4, ease: "ease-in-out" },"+=4")
+        .to('.div3 h1', {opacity: 1, duration: 8, ease: "ease-in-out"})
+    }
+})
 
 
 
