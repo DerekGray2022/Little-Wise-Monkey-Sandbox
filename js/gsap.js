f2aa1f32f4804b62cb426bf07c01fe1c056578bb
Derek Gray <105393523+DerekGray2022@users.noreply.github.com>
2024-03-17 17:36:24 +0000

    17 Mar '24

--- js/gsap.js
+++ js/gsap.js
@@ -1,40 +1,51 @@
 gsap.registerPlugin(ScrollTrigger);
 
 //      onLoad Animation
-let tl = gsap.timeline();
-tl.to('#header', { opacity: 1, y: 100, duration: 2, ease: 'inOut' });
-tl.to('#about', { opacity: 1, x: 200, ease: "elastic", duration: 3 });
-tl.to('#employment', { opacity: 1, x: 200, ease: "elastic", duration: 3 }, "-=2.5");
-tl.to('#projects', { opacity: 1, x: 200, ease: "elastic", duration: 3 }, "-=2.5");
-tl.to('.intro', { opacity: 1, y: -50, duration: 2 }, "-=2.5");
+let tl = gsap.timeline()
+.to('#header', { opacity: 1, y: 100, duration: 2, ease: 'inOut' })
+.to('#about', { opacity: 1, x: 200, ease: "elastic", duration: 3 })
+.to('#employment', { opacity: 1, x: 200, ease: "elastic", duration: 3 }, "-=2.5")
+.to('#projects', { opacity: 1, x: 200, ease: "elastic", duration: 3 }, "-=2.5")
+.to('.intro', {opacity: 1, y: -50, duration: 2 }, "-=2.5") 
+.to('#arrow', {opacity: 0.25, duration: 2}, "-=1")
+
 
 
 //      Scroll Animation
 ScrollTrigger.matchMedia({
 
+    //      GALAXY FOLD SCREENS
     "(max-width: 320px)": () => {
+        //      Scroll down to Page Two
         let tl = gsap.timeline({
             scrollTrigger: {
                 trigger: ".div2",
-                // markers: true,
+                markers: true,
                 start: "top 90%",
                 end: "top top",
                 scrub: 4,
+                pin: true,
                 isTouch: 2
             }
         });
+        //      Intro Paragraph
         tl.to('.intro p', {
             y: 350,
             x: -20,
-            duration: 5,
+            duration: 3,
             ease: "ease-out",
             color: "#a9029e",
             width: "80vw",
         })
-        .to('.hidden-xsm', { opacity: 1, duration: 4 })
-        .to('.div1 h1', {opacity: 1, duration: 8, ease: "ease-in-out"})
+        //      About Header
+        .to('.div1 h1', { opacity: 1, duration: 3, ease: "ease-in-out" })
+        //      Arrow
+        .to('#arrow', { y: window.innerHeight, duration: 5 }, "-=2")
+        //      About Extra Paragraphs
+        .to('.hidden-xsm', { opacity: 1, duration: 2 });
     },
 
+    //      IPHONE + ABOVE
     "(min-width: 321px)": () => {
         let tl = gsap.timeline({
             scrollTrigger: {
@@ -42,7 +53,8 @@ ScrollTrigger.matchMedia({
                 // markers: true,
                 start: "top 90%",
                 end: "top top",
-                scrub: 4,
+                scrub: 10,
+                pin:true,
                 isTouch: 2
             }
         });
@@ -54,9 +66,10 @@ ScrollTrigger.matchMedia({
             color: "#a9029e",
             width: "80vw",
         })
-        .to('.hidden-xsm', { opacity: 1, duration: 4, ease: "ease-in-out" },"+=4")
-        .to('.hidden-sm', { display: "block",opacity: 1, duration: 4, ease: "ease-in-out" },"+=4")
-        .to('.div3 h1', {opacity: 1, duration: 8, ease: "ease-in-out"})
+        .to('.div3 h1', { opacity: 1, duration: 4, ease: "ease-in-out" }, "-=3")
+        .to('#arrow', { y: window.innerHeight, duration: 5 }, "-=2")
+        .to('.hidden-xsm', { opacity: 1, duration: 2, ease: "ease-in-out" }, "-=4")
+        .to('.hidden-sm', { display: "block", opacity: 1, duration: 2, ease: "ease-in-out" }, "-=4");
     }
 })
 
